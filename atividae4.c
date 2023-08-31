f2221b4a9dce89e2b01199547d7291bc2a48f647
clarice <142429120+clarisv@users.noreply.github.com>
2023-08-31 11:08:41 -0300

    Update atividae4.c

---
+++ atividae4.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+main(){
+
+//leia o peso e imprima se é menor que 60kg
+
+ double peso;
+
+ printf("digite o peso");
+ scanf("%lf", &peso);
+
+ if(peso <60.0){
+    printf("peso menor que 60");
+
+
+ }else{
+    printf("peso maior que 60");
+
+ }
+
+}
